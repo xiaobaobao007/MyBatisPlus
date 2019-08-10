796be1da0e10bb14289e22e6b252cb8eff2a5bd7
xiaobaobao007 <932824098@qq.com>
2019-08-10 16:51:18 +0800

    mybatis不是mybatis-plus

---
+++ src/test/java/MyBatisTest.java
@@ -0,0 +1,40 @@
+/**
+ * @author xiaobaobao
+ * @date 2019/8/10 16:19
+ */
+import model.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class MyBatisTest {
+    @Test
+    public void userFindByIdTest() {
+        //定义读取文件名
+        String resources = "mybatis-config.xml";
+        //创建流
+        Reader reader = null;
+        try {
+            //读取mybatis-config.xml文件到reader对象中
+            reader = Resources.getResourceAsReader(resources);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //初始化mybatis,创建SqlSessionFactory类的实例
+        SqlSessionFactory sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+        //创建session实例
+        SqlSession session = sqlMapper.openSession();
+        //传入参数查询，返回结果
+        User user = session.selectOne("findById", 1);
+        //输出结果
+        System.out.println(user.getName());
+        //关闭session
+        session.close();
+
+    }
+}
